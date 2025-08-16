557568e05f722b2dd352fbb4b55ba62d9adea73c
yudofusite <yasutaka82@icloud.com>
2025-08-16 16:07:38 +0900

    Update general.js

--- general.js
+++ general.js
@@ -2,6 +2,8 @@ const me_b = document.getElementById("menubtn");
 const me_v = document.getElementById("sider");
 let menu = 0;    //メニューが開いているかどうか。1=展開
 
+me_v.innerHTML = '<div class="sider" id="sider"> <a href="index.html">湯豆腐サイトトップ</a><br><a href="select-page.html">記事一覧</a><br><br><a href="https://yudofusite.github.io/japan_desktop/">日本を飛ばそう Desktop Edition</a><br><br><a href="https://youtube.com/channel/UCtSopuVkfDc34jFfcvp0-NA?si=kNI5QTj2ntiVSWMc">YouTubeチャンネル</a><br><a href="https://scratch.mit.edu/users/kokesiant">scratchマイページ</a><hr><small>&copy;2025 yudofusite管理人 kokesiant</small> </div>';
+
 me_b.addEventListener('mousedown', () => {
   if (menu == 1) {
     menu = 0;
@@ -31,4 +33,5 @@ let footer = document.getElementById("footer");
 if (footer != null) {
   footer.innerHTML = '<div class="footer_gen"><b><a href="index.html">湯豆腐サイト</a></b><br>湯豆腐のかたまり ── Assemble Yudofu</div>';
 }
+
 
