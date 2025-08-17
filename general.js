ac0240f45844a70bc23a52d5cd36b52249200609
yudofusite <yasutaka82@icloud.com>
2025-08-17 16:17:08 +0900

    Update general.js

--- general.js
+++ general.js
@@ -5,7 +5,8 @@ let menu = 0;    //メニューが開いているかどうか。1=展開
 window.onload = function(){
   const head = document.head;
   const favicon = '<link rel="icon" href="favicon.ico">';
-  head.innerHTML = head + favicon
+  head.innerHTML = head + favicon;
+  console.log("アイコン設定");
 }
 
 me_vb.innerHTML = '<div class="sider" id="sider"> <a href="index.html">湯豆腐サイトトップ</a><br><a href="select-page.html">記事一覧</a><br><br><a href="https://yudofusite.github.io/japan_desktop/">日本を飛ばそう Desktop Edition</a><br><br><a href="https://youtube.com/channel/UCtSopuVkfDc34jFfcvp0-NA?si=kNI5QTj2ntiVSWMc">YouTubeチャンネル</a><br><a href="https://scratch.mit.edu/users/kokesiant">scratchマイページ</a><hr><small>&copy;2025 yudofusite管理人 kokesiant</small> </div>';
@@ -54,6 +55,7 @@ if (footer != null) {
     この4行を</head>の前に書いておくこと
 
 */
+
 
 
 
